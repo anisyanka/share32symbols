c39cb29d759a927ad6dcabcb24aa8956b6e26a3d
Aleksandr Anisimov <anisimov.alexander.s@gmail.com>
2022-12-26 00:30:31 +0100

    [fw] Add OLED device adapter code

--- fw/Core/main.c
+++ fw/Core/main.c
@@ -3,6 +3,7 @@
 #include "init.h"
 #include "usb_device.h"
 #include "usbd_cdc_if.h"
+#include "ws0010.h"
 
 #include <stdbool.h>
 
@@ -13,6 +14,58 @@ char oled_line2[16] = { 0 };
 bool is_package_received = false;
 uint8_t user_rx_buffer[APP_USER_RX_BUFFER_SIZE] = { 0 };
 
+ws0010_ll_t oled_ll_func = { 0 };
+ws0010_dev_t oled_dev = {
+	.line_count = 2,
+	.displayed_symbols_per_line = 16,
+	.max_symbols_per_line = 64,
+	.font_size = WS0010_5x8_DOTS,
+	.interface_bits = WS0010_8_BITS,
+	.alphabet = ENG_RUS,
+	.ll = &oled_ll_func,
+};
+
+static void oled_delay_us(uint32_t)
+{
+	/* HARDCODE */
+	HAL_Delay(50);
+}
+
+static void oled_out_bits(uint8_t value)
+{
+	GPIOA->ODR = value;
+}
+
+static void oled_set_rs(void)
+{
+	HAL_GPIO_WritePin(OLED_RS_GPIO_Port, OLED_RS_Pin, GPIO_PIN_SET);
+}
+
+static void oled_reset_rs(void)
+{
+	HAL_GPIO_WritePin(OLED_RS_GPIO_Port, OLED_RS_Pin, GPIO_PIN_RESET);
+}
+
+static void oled_set_rw(void)
+{
+	HAL_GPIO_WritePin(OLED_RW_GPIO_Port, OLED_RW_Pin, GPIO_PIN_SET);
+}
+
+static void oled_reset_rw(void)
+{
+	HAL_GPIO_WritePin(OLED_RW_GPIO_Port, OLED_RW_Pin, GPIO_PIN_RESET);
+}
+
+static void oled_set_e(void)
+{
+	HAL_GPIO_WritePin(OLED_E_GPIO_Port, OLED_E_Pin, GPIO_PIN_SET);
+}
+
+static void oled_reset_e(void)
+{
+	HAL_GPIO_WritePin(OLED_E_GPIO_Port, OLED_E_Pin, GPIO_PIN_RESET);
+}
+
 int main(void)
 {
 	HAL_Init();
@@ -20,6 +73,17 @@ int main(void)
 	GPIO_Init();
 	USB_DEVICE_Init();
 	USART1_UART_Init();
+
+	oled_ll_func.delay_us = oled_delay_us;
+	oled_ll_func.set_bits_to_out_pins = oled_out_bits;
+	oled_ll_func.set_rs = oled_set_rs;
+	oled_ll_func.reset_rs = oled_reset_rs;
+	oled_ll_func.set_rw = oled_set_rw;
+	oled_ll_func.reset_rw = oled_reset_rw;
+	oled_ll_func.set_e = oled_set_e;
+	oled_ll_func.reset_e = oled_reset_e;
+
+	ws0010_init(&oled_dev);
 
 	while (1)
 	{
